27e098f66c64b307f9e210a3bd7488c791b795fe
Kayra <KayraAlat@gmail.com>
2015-11-21 17:04:49 +0000

    Added get female view

--- server/fantasticfem/females/views.py
+++ server/fantasticfem/females/views.py
@@ -7,13 +7,21 @@ from .serializers import FemaleSerializer
 
 
 @api_view(['GET'])
-def getFemale(reuqest):
-    pass
+def getFemale(request):
+
+    try:
+        female = Female.objects.get(pk=request.data['pk'])
+    except Female.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializedFemale = FemaleSerializer(female)
+    return Response(serializedFemale.data)
 
 
 @api_view(['GET'])
-def getRandomFemale(reuqest):
+def getRandomFemale(request):
     pass
+
 
 
 @api_view(['POST'])

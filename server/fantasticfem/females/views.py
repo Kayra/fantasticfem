5c429996f13e17fdf73eb5f04f1da3f1b616aac1
Kayra <KayraAlat@gmail.com>
2015-11-21 16:43:45 +0000

    Added views structure

--- server/fantasticfem/females/views.py
+++ server/fantasticfem/females/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Female
+from .serializers import FemaleSerializer
+
+
+@api_view(['GET'])
+def getFemale(reuqest):
+    pass
+
+
+@api_view(['GET'])
+def getRandomFemale(reuqest):
+    pass
+
+
+@api_view(['POST'])
+def createFemale(request):
+    pass
+
+
+@api_view(['POST'])
+def editFemale(request):
+    pass
+
+
+@api_view(['POST'])
+def deleteFemale(request):
+    pass

2c6545273a8d108f84b240a7df4d30ccea9e7458
Kayra <KayraAlat@gmail.com>
2015-11-21 21:28:25 +0000

    Added sass and watch tasks to grunt file

--- client/gruntfile.js
+++ client/gruntfile.js
@@ -1,4 +1,25 @@
 
 module.exports = function(grunt) {
+    grunt.initConfig({
+
+        watch: {
+            sass: {
+                files: "styles/sass/*.sass",
+                tasks: ['sass']
+            }
+        },
+
+        sass: {
+            dev: {
+                files: {
+                    "styles/css/styles.css" : "styles/sass/styles.sass"
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
 };

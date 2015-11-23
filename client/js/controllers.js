f94a1a8c59227013cc11bbc406453eb88ccb0fd1
Kayra <KayraAlat@gmail.com>
2015-11-23 05:08:49 +0000

    Added controllers structure

---
+++ client/js/controllers.js
@@ -0,0 +1,17 @@
+(function(){
+
+    var femaleAppControllers = angular.module('femaleApp.controllers', [])
+
+    .controller('FemaleDisplayController', function(){
+
+    })
+
+    .controller('FemaleCreateController', function(){
+
+    })
+
+    .controller('FemaleEditController', function(){
+
+    });
+
+})();

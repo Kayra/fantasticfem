05a7beab29be02b18ff0ed99c7cb8f673d52a624
Kayra <KayraAlat@gmail.com>
2015-11-23 05:21:20 +0000

    Added female display controller with dummy data

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -4,6 +4,19 @@
 
     .controller('FemaleDisplayController', function(){
 
+        var ctrl = this;
+
+        ctrl.getRandomFemale = function(){
+            ctrl.first_name = 'Grace';
+            ctrl.last_name = 'Hopper';
+            ctrl.date_of_birth = '9/12/06';
+            ctrl.zip_code = '10001';
+            ctrl.bio = 'Grace was an American computer scientist and United States Navy Rear Admiral.';
+            ctrl.fantastic_bio = 'Absolute champion of programming. The first ever compiler? That was Grace. The term debugging? That was Grace. Implementation of the first testing standards? You know exactly who. Basically you wouldn\'t be reading this if not for Grace. At least not with technology as advanced as what we have available today.';
+        };
+
+        ctrl.getRandomFemale();
+
     })
 
     .controller('FemaleCreateController', function(){

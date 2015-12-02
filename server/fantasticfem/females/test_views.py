f23a6b07cf0d6e3de09eb89b78ae5cef9916d825
Kayra <KayraAlat@gmail.com>
2015-12-02 22:24:37 +0000

    Server - Added structure for the view tests

---
+++ server/fantasticfem/females/test_views.py
@@ -0,0 +1,29 @@
+import json
+
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from .models import Female
+
+class FemaleAPITests(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_getRandomFemale(self):
+        pass
+
+    def test_getFemale(self):
+        pass
+
+    def test_getFemaleList(self):
+        pass
+
+    def test_createFemale(self):
+        pass
+
+    def test_editFemale(self):
+        pass
+
+    def test_deleteFemale(self):
+        pass

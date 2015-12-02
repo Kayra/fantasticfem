d6e002a35dc82b9a150417df19a8684181c9fca1
Kayra <KayraAlat@gmail.com>
2015-12-02 18:46:12 +0000

    Client - Added directives.js

---
+++ client/js/directives.js
@@ -0,0 +1,26 @@
+(function(){
+
+    var femaleAppDirectives = angular.module('femaleApp.directives', []);
+
+    femaleAppDirectives.directive('ngFocus', [function() {
+
+        var focusClass = "ng-focused";
+
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, element, attrs, ctrl) {
+                ctrl.$focused = false;
+                element.bind('focus', function(evt) {
+                    element.addClass(focusClass);
+                    scope.$apply(function() {ctrl.$focused = true;});
+                }).bind('blur', function(evt) {
+                    element.removeClass(focusClass);
+                    scope.$apply(function() {ctrl.$focused = false;});
+                });
+            }
+        }
+
+    }]);
+
+})();

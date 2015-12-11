486466d65ae0c6ccfab1548be827fe3a5c9d4ef4
Kayra <KayraAlat@gmail.com>
2015-12-11 18:59:11 +0000

    Server - Added to get female test

--- server/females/test_views.py
+++ server/females/test_views.py
@@ -6,29 +6,37 @@ from django.test import TestCase
 from .models import Female
 
 
-def createFemale(firstName, lastName, dateOfBirth, zipCode, bio, fantasticBio):
+def createFemale(firstName, lastName, dateOfBirth, zipCode):
     """
         Convinience function to create a test female
     """
-    Female.objects.create(firstName=firstName, lastName=lastName, dateOfBirth=dateOfBirth, zipCode=zipCode, bio=bio, fantasticBio=fantasticBio)
+    Female.objects.create(firstName=firstName, lastName=lastName, dateOfBirth=dateOfBirth, zipCode=zipCode, bio='bio example', fantasticBio='fantastic bio example')
 
 
 class FemaleAPITests(TestCase):
 
     def setUp(self):
-        createFemale('Kayra', 'Alat', '1990-01-01', '11111', 'bio example', 'fantasticbio example')
-        createFemale('Oytun', 'Alat', '1990-01-01', '11111', 'bio example', 'fantasticbio example')
-        createFemale('Sule', 'Alat', '1990-01-01', '11111', 'bio example', 'fantasticbio example')
+        createFemale('Ada', 'Lovelace', '1990-01-01', '11111')
+        createFemale('Barbara', 'Liskov', '1990-02-02', '22222')
+        createFemale('Murasaki', 'Shikibu', '1990-02-02', '33333')
 
     def test_getFemale(self):
+
         """
             A female is returned when a correct Id is passed as a parameter
         """
+
+        testFemale = Female.objects.all()[:1].get()
+        testIdentifier = testFemale.id
+
         url = reverse('females:get_female')
-        response = self.client.get(url, {'identifier': 10})
+        response = self.client.get(url, {'identifier': testIdentifier})
         self.assertEquals(response.status_code, 200)
+
         data = json.loads(response.content.decode())
         self.assertEquals(len(data), 7)  # Make sure all fields are present
+
+        self.assertEquals(testIdentifier, data['id'])  # Make sure the correct female was returned
 
     def test_getRandomFemale(self):
         """

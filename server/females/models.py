902f922c5ae46188829b2b9d7441ec7b630651a2
kayra <KayraAlat@gmail.com>
2015-12-14 18:33:37 +0000

    Server - Added profileImage imagefield to female model

--- server/females/models.py
+++ server/females/models.py
@@ -5,6 +5,6 @@ class Female(models.Model):
 
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
-    # image = models.ImageField()
+    profileImage = models.ImageField()
     bio = models.TextField()
     fantasticBio = models.TextField()

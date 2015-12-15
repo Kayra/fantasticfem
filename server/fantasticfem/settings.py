0dc3143db6a781c9c5a80cc5cdbd7be42c4459d9
kayra <KayraAlat@gmail.com>
2015-12-15 18:05:09 +0000

    Server - Added media folder into settings

--- server/fantasticfem/settings.py
+++ server/fantasticfem/settings.py
@@ -107,8 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = 'static/'
+MEDIA_ROOT = 'media/'
 
 # Rest framework settings
 

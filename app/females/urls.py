aaa351de59f10f93f1b29868d9d93f184db5788c
Kayra <KayraAlat@gmail.com>
2016-07-15 20:15:11 +0100

    Added random female view (homepage)

--- app/females/urls.py
+++ app/females/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from . import views
+from females import views
 
 urlpatterns = [
 
-    url(r'^list/?$', views.FemaleList.as_view(), name='female_list')
+    url(r'^', views.FemaleRandom.as_view(), name='female_random'),
+    url(r'^list/?$', views.FemaleList.as_view(), name='female_list'),
 
 ]
--- app/females/views.py
+++ app/females/views.py
@@ -1,7 +1,18 @@
+from django.views.generic.base import TemplateView
 from django.views.generic import ListView
 
 from females.models import Female
-from females import utility
+from females import utils
+
+
+class FemaleRandom(TemplateView):
+
+    template_name = 'female_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(FemaleRandom, self).get_context_data(**kwargs)
+        context['female'] = utils.get_random_female()
+        return context
 
 
 class FemaleList(ListView):
